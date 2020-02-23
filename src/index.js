bcabc6b533d2d40da7fbdec9dff1ea9a511075fc
AzamatovUtkirbek <58224813+AzamatovUtkirbek@users.noreply.github.com>
2020-02-23 19:47:24 +0500

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if((typeof array==='undefined')||(array.length==0)){
+    return 0;
+  }else{
+    var t=array[0];
+    for(let item of array){
+      if(item<t){
+        t=item;
+      }
+    }
+    return t;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if((typeof array==='undefined')||(array.length == 0)){
+    return 0;
+  }else{
+    var t=array[0];
+    for(let item of array){
+      if(item>t){
+        t=item;
+      }
+    }
+    return t;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if((typeof array==='undefined')||(array.length == 0)){
+    return 0;
+  }else{
+    var sum=0;
+    for(let item of array){
+      sum+=item;
+    }
+    return sum/array.length;
+  }
 }
